965e0a0323ae6cc69246359f41014f24b185cdb0
Nikhil Nikam <nikhilnikam619@gmail.com>
2024-01-06 18:02:05 +0530

    app completed

--- app.js
+++ app.js
@@ -7,13 +7,39 @@ let btnTranslet = document.querySelector("#btn-translet")
 let txtinput = document.querySelector("#btn-input")
 let output = document.querySelector("#output")
 
+//let url1 = "https://api.funtranslations.com/translate/yoda.json/"
 
+let url = "https://api.funtranslations.com/translate/minion.json"
+function gettranslationurl(text) {
+
+    return url + "?" + "text=" + text
+
+}
+
+function errorhandler(error) {
+
+    console.log("their is error try aftersome time", error)
+}
 
 function clickHandler() {
 
-    console.log("clicked");
-    console.log("input", txtinput.value);
-    output.innerText = txtinput.value;
+    // console.log("clicked");
+    //console.log("input", txtinput.value);
+    //output.innerText = txtinput.value;
+
+    let inputtext = txtinput.value;
+
+    fetch(gettranslationurl(inputtext))
+        .then(response => response.json())
+        .then(json => {
+            let translatedtxt = json.contents.translated;
+
+            output.innerText = translatedtxt;
+
+        })
+        .catch(errorhandler)
+
+
 
 
 }
